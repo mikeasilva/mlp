c89127a9b82e68409c3ae441633e3740d6083a21
mikeasilva <mike.a.silva@gmail.com>
2023-01-28 20:57:28 -0500

    Updates to data.py

--- MLP/data.py
+++ MLP/data.py
@@ -1,9 +1,11 @@
-def generate_soap_notes(n:int) -> list:
+def generate_soap_notes(n:int, anonymize_patient:bool=True, use_full_section_headers:bool=False) -> list:
     '''
     Generates synthetic SOAP notes.
 
             Parameters:
                     n (int): The number of SOAP notes to generate
+                    anonymize_patient (bool): Should the patient name be anonymous (default True)
+                    use_full_section_headers (bool): Produce SOAP notes with a full text section header (default False)
 
             Returns:
                     soap_notes (list): A list of strings
@@ -12,7 +14,13 @@ def generate_soap_notes(n:int) -> list:
 
     ## Assure that n is an integer
     if not isinstance(n, int):
-        raise TypeError("Expected int. Received " + type(n).__name__)
+        raise TypeError("n must be an int. Received a " + type(n).__name__)
+    ## Assure that anonymize_patient is a bool
+    if not isinstance(anonymize_patient, bool):
+        raise TypeError("anonymize_patient must be a bool. Received a " + type(anonymize_patient).__name__)
+    ## Assure that use_full_section_headers is a bool
+    if not isinstance(use_full_section_headers, bool):
+        raise TypeError("use_full_section_headers must be a bool. Received a " + type(use_full_section_headers).__name__)
     
     ## Assure the user requests a reasonable number of SOAP notes
     if n >= 1000000:
@@ -21,13 +29,16 @@ def generate_soap_notes(n:int) -> list:
         raise ValueError("Too few SOAP notes requested!")
     
     # Get a template to fill in
-    soap_template = r"Subjective\n[subjective}]nObjective\n[objective]\Assessment\n[assessment}]nPlan\n[plan]"
+    if use_full_section_headers:
+        soap_template = "Subjective\n[subjective]\nObjective\n[objective]\nAssessment\n[assessment]\nPlan\n[plan]"
+    else:
+        soap_template = "S: [subjective]\nO: [objective]\nA: [assessment]\nP: [plan]"
 
     # Read in data from the SNOMED CT problems list
 
     # Create the SOAP notes
     soap_notes = []
-    for i in range(0, n):
+    for i in range(n):
         subjective = "s"
         objective = "o"
         assessment = "a"
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,26 +1,59 @@
 import pytest
 from MLP import data
 
-def test_data_generate_soap_notes_raises_value_error_with_big_number():
+def test_generate_soap_notes_raises_value_error_with_big_number():
     with pytest.raises(ValueError, match="Too many SOAP notes requested!"):
         data.generate_soap_notes(1000000)
 
-def test_data_generate_soap_notes_raises_value_error_with_negative_number():
+def test_generate_soap_notes_raises_value_error_with_negative_number():
     with pytest.raises(ValueError, match="Too few SOAP notes requested!"):
         data.generate_soap_notes(-1)
 
-def test_data_generate_soap_notes_raises_type_error_when_junk_passed_in():
-    error = "Expected int. Received "
+def test_generate_soap_notes_raises_type_error_when_junk_passed_in():
+    error = "n must be an int. Received a "
     with pytest.raises(TypeError, match=error + "str"):
         data.generate_soap_notes("1")
     with pytest.raises(TypeError, match=error + "dict"):
         data.generate_soap_notes({"1":"1"})
     with pytest.raises(TypeError, match=error + "list"):
         data.generate_soap_notes([1])
+    with pytest.raises(TypeError, match=error + "float"):
+        data.generate_soap_notes(1/2)
 
-def test_data_generate_soap_notes_returns_a_list():
+    # Pass in invalid anonymize_patient
+    error = "anonymize_patient must be a bool. Received a "
+    with pytest.raises(TypeError, match=error + "str"):
+        data.generate_soap_notes(1, anonymize_patient = "1")
+    with pytest.raises(TypeError, match=error + "dict"):
+        data.generate_soap_notes(1, anonymize_patient = {"1":"1"})
+    with pytest.raises(TypeError, match=error + "list"):
+        data.generate_soap_notes(1, anonymize_patient = [1])
+    with pytest.raises(TypeError, match=error + "float"):
+        data.generate_soap_notes(1, anonymize_patient = 1/2)
+
+    # Pass in invalid use_full_section_headers
+    error = "use_full_section_headers must be a bool. Received a "
+    with pytest.raises(TypeError, match=error + "str"):
+        data.generate_soap_notes(1, use_full_section_headers = "1")
+    with pytest.raises(TypeError, match=error + "dict"):
+        data.generate_soap_notes(1, use_full_section_headers = {"1":"1"})
+    with pytest.raises(TypeError, match=error + "list"):
+        data.generate_soap_notes(1, use_full_section_headers = [1])
+    with pytest.raises(TypeError, match=error + "float"):
+        data.generate_soap_notes(1, use_full_section_headers = 1/2)
+
+
+def test_generate_soap_notes_returns_a_list():
     assert isinstance(data.generate_soap_notes(1), list)
 
-def test_data_generate_soap_notes_returns_correct_number_of_notes_in_a_list():
+def test_generate_soap_notes_returns_correct_number_of_notes_in_a_list():
     assert len(data.generate_soap_notes(1)) == 1
     assert len(data.generate_soap_notes(2)) == 2
+
+def test_generate_soap_notes_replaces_template_placeholders():
+    soap_note = data.generate_soap_notes(1)[0]
+    n_characters = len(soap_note)
+    assert len(soap_note.replace("[subjective]", "")) == n_characters
+    assert len(soap_note.replace("[objective]", "")) == n_characters
+    assert len(soap_note.replace("[assessment]", "")) == n_characters
+    assert len(soap_note.replace("[plan]", "")) == n_characters
